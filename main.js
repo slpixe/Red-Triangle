af40b4531976a4992e9c12d2ddf96cd3fe5d9566
Slpixe <slpixe@gmail.com>
2017-11-05 02:19:06 +0000

    Core Functionality - slider, search pane, search results, play search result video (#2)
    * seek bar grow width
    
    * add console_log debug info
    
    * tweak to allow slider to reach end by rounding-up/ceil video current time
    
    * start of search pane
    
    * local placeholder image. add open search pane button event
    
    * add font awesome and some search pane styles
    
    * allow passing through values (will be used for api key during testing/development
    
    * start of search items template
    
    * search pane and tool bar fixed to prevent scrolling to see them
    
    * search pane show 1 2 or 4 tiles per row depending on screensize
    
    * on entering search term, perform search with youtube, on scroll request the next page
    
    * load up video on clicking it from the search-results

--- main.js
+++ main.js
@@ -1,21 +1,40 @@
 "use strict";
 
-console.clear();
+// console.clear();
 
 /* ============= Global Variables ============= */
 var state = null;
 var player = null;
-var slider = document.getElementById("seek");
-var elPlayingStatus = document.getElementById("playingStatus");
 var videoDuration = null; //total video length seconds
 var userActive = false;
 var userActiveTimer = null;
 var sliderInUse = false;
+var DEBUG = true;
+var lastKeystroke = null;
+var nextPageToken = null;
+var isSearching = null;
+var url = new URL(window.location);
+var API_KEY_YOUTUBE = url.searchParams.get("API_KEY");
+
+var youtubeApiUrl = 'https://www.googleapis.com/youtube/v3/';
+
+//Elements
+var slider = document.getElementById("seek");
+var elPlayingStatus = document.getElementById("playingStatus");
+var elOpenSearch = document.querySelector('.search');
+var elSearchPane = document.querySelector('.search-pane');
+var elSearchInput = document.querySelector('.search-input');
+var elSearchSubmitBtn = document.querySelector('.search-submit');
+var elSearchResults = document.querySelector('.search-results');
 
 //config values
 var VOLUME = 0; //percent
 var INTERVAL_USER_ACTIVE = 3000;
 var INTERVAL_SLIDER = 50; // in ms
+// var DEBUG_TIMER = 5000;
+var DEBUG_TIMER = 60000;
+var SCROLL_TRIGGER = 300;  // in pixels, before hitting the bottom
+var SEARCH_DELAY = 1000;  // in milliseconds
 
 //passeed through values (E.g. get parameters from url
 // www.mysite.com/my_app.html?Use_Id=abc
@@ -29,6 +48,15 @@ for (var i = 0, max = query.length; i < max; i++) {
   GET[decodeURIComponent(param[0])] = decodeURIComponent(param[1] || "");
 }
 
+/*========TEMPLATES==========*/
+
+const SearchItem = ({title, videoId, thumbnail}) => `
+  <li class="search-result-item" data-videoid="${videoId}">
+    <img src="${thumbnail}" alt="" />
+    <strong>${title}</strong>
+  </li>
+`;
+
 /*========FUNCTIONS==========*/
 
 function stopVideo () {
@@ -36,7 +64,7 @@ function stopVideo () {
 }
 
 function togglePause () {
-  if(state == YT.PlayerState.PLAYING) {
+  if (state == YT.PlayerState.PLAYING) {
     player.pauseVideo();
     // elPause
   } else {
@@ -130,32 +158,136 @@ function updateSlider () {
 
   if (state != YT.PlayerState.PLAYING) return;
 
-  slider.value = player.getCurrentTime();
+  try {
+    slider.value = Math.ceil(player.getCurrentTime());
+  } catch (e) {}
 }
 
 function hideToolboxCountdown () {
   clearTimeout(userActiveTimer);
   userActiveTimer = setTimeout(userInactive, INTERVAL_USER_ACTIVE);
-  // 1000 milisec = 1 sec
 }
 
 function userInactive () {
-  console.log('userInactive');
-  hideToolbox();
   userActive = false;
+  if (DEBUG)
+    console.log('userInactive');
+  hideToolbox();
+  searchHide();
 }
 
 function showToolbox () {
-  if (!userActive) {
-    document.querySelector('.tools').classList.add('show');
-  }
+  document.querySelector('.tools').classList.add('show');
 }
 
 function hideToolbox () {
   document.querySelector('.tools').classList.remove('show');
 }
 
+function searchShow () {
+  if (DEBUG)
+    console.log('showing search');
+  document.querySelector('.search-pane').classList.add('show');
+}
+
+function onSearchScroll (el) {
+  if (el.scrollTop + el.clientHeight + SCROLL_TRIGGER >= el.scrollHeight) {
+    if (DEBUG)
+      console.log('do search based on scrolling (no delay and nextPage)');
+    search(true, true);
+  }
+}
+
+function searchHide () {
+  if (DEBUG)
+    console.log('hiding search');
+  document.querySelector('.search-pane').classList.remove('show');
+}
+
+function debugInfo () {
+  console.clear();
+  console.log("DEBUG_TIMER: " + DEBUG_TIMER);
+  console.log("state: " + state);
+  console.log("videoDuration: " + videoDuration);
+  console.log("userActive: " + userActive);
+  console.log("userActiveTimer: " + userActiveTimer);
+  console.log("sliderInUse: " + sliderInUse);
+  console.log("VOLUME: " + VOLUME);
+  console.log("INTERVAL_USER_ACTIVE: " + INTERVAL_USER_ACTIVE);
+  console.log("INTERVAL_SLIDER: " + INTERVAL_SLIDER);
+}
+
+function searchInputUpdated (e) {
+  if (e.key === 'Enter') {
+    search(true)
+  }
+
+  var date = new Date();
+  lastKeystroke = date.getTime();
+  setTimeout(search, SEARCH_DELAY);
+}
+
+function search (noDelay, nextPage) {
+  //quit out if theres no search value
+  if (elSearchInput.value.length === 0) return;
+
+  //if called by keyup check that longer than the delay has passed, otherwise cancel out
+  var date = new Date();
+  if (!noDelay && date.getTime() - lastKeystroke < SEARCH_DELAY)
+    return;
+
+  //if already searching (e.g. got called again because rapid scroll hits then cancel out
+  if (isSearching)
+    return;
+
+  if (DEBUG)
+    console.log('noDelay = ' + noDelay);
+
+  if (DEBUG)
+    console.log('nextPage = ' + nextPage);
+  fetchSearchFromYoutube(elSearchInput.value, nextPage);
+}
+
+function fetchSearchFromYoutube (query, nextPage) {
+  var encodedQuery = encodeURIComponent(query);
+
+  var url = youtubeApiUrl + 'search?q=' + encodedQuery + '&type=video&part=snippet&maxResults=10&key=' + API_KEY_YOUTUBE;
+
+  if (nextPage && nextPageToken)
+    url += '&pageToken=' + nextPageToken;
+
+  if (DEBUG)
+    console.log(url);
+
+  isSearching = true;
+
+  fetch(url)
+    .then(response => response.json())
+    .then((response) => {
+      if (DEBUG) {
+        console.log(response);
+      }
+
+      nextPageToken = response.nextPageToken;
+
+      response.items.map(x => {
+        const item = SearchItem({
+          title: x.snippet.title,
+          videoId: x.id.videoId,
+          thumbnail: x.snippet.thumbnails.high.url
+        });
+        document.querySelector('.search-results').innerHTML += item;
+      });
+
+      isSearching = false;
+    });
+}
+
 /*=======Events========*/
+
+if (DEBUG) {
+  setInterval(debugInfo, DEBUG_TIMER);
+}
 
 //api calls this once loaded
 function onYouTubeIframeAPIReady () {
@@ -165,12 +297,28 @@ function onYouTubeIframeAPIReady () {
   setInterval(updateSlider, INTERVAL_SLIDER);
 }
 
-function handleMouseMove () {
+function handleUserActive () {
+  userActive = true;
   showToolbox();
   hideToolboxCountdown();
-  userActive = true;
 }
 
-document.onmousemove = handleMouseMove;
+function handleSearchItemResultClick (e) {
+  if(e.target && e.target.parentNode.className === 'search-result-item')
+    if(e.target.parentNode.dataset.videoid)
+      loadVideo(e.target.parentNode.dataset.videoid);
+      searchHide();
+}
+
+document.onmousemove = handleUserActive;
+// not working for some reason
+// document.onscroll = handleUserActive;
 
 elPlayingStatus.addEventListener('click', togglePause);
+elOpenSearch.addEventListener('click', searchShow);
+elSearchInput.addEventListener('keyup', function (e) { searchInputUpdated(e); });
+elSearchSubmitBtn.addEventListener('click', function () {search(true)});
+elSearchResults.addEventListener('click', function (e) { handleSearchItemResultClick(e); });
+
+//not working for some reason
+// elSearchPane.addEventListener('scroll', onSearchScroll(this));
